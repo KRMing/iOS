e737aa8b19603052bf85cee0a5f0097921e3a83a
KRMing <mkleeboy3@naver.com>
2020-02-23 18:16:59 +0900

    completed lesson 5 and lesson 5 exercise

---
+++ Lesson5.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// declaring a variable
+var str:String = "Hello, playground"
+print(str)
+str = "some data"
+print(str)
+
+// declaring a constant
+let con = "more data"
+
+// data types
+var bool:Bool = false
+print(bool)
+
+var i:Int = 32
+i = 0
+i = -10
+
+var f:Double = 0.34758
