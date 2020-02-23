b3564b38737def244b6c0f2a3d962a882fe35453
KRMing <mkleeboy3@naver.com>
2020-02-23 13:36:45 +0900

    uploaded the iOS project files to Github for the first time

---
+++ loginscreen/loginscreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loginscreen
+//
+//  Created by 이민건 on 2/22/20.
+//  Copyright © 2020 이민건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
