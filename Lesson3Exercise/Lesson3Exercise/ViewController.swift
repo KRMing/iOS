0dce112ac711c284c0034da5f9f66ddfbf00cb76
KRMing <mkleeboy3@naver.com>
2020-02-23 15:35:11 +0900

    completed lesson 3 exercise

---
+++ Lesson3Exercise/Lesson3Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson3Exercise
+//
+//  Created by 이민건 on 2/23/20.
+//  Copyright © 2020 이민건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
