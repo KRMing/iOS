d9505ab3806e54d9b25073733e32eb60edc37f6a
KRMing <mkleeboy3@naver.com>
2020-02-25 11:11:25 +0900

    completed lesson 6 (Implementing the Protocol & Delegate Pattern)

--- Module 3 The Card Game/MatchApp/MatchApp/ViewController.swift
+++ Module 3 The Card Game/MatchApp/MatchApp/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var collectionView: UICollectionView!
     
     let model = CardModel()
     var cardsArray = [Card]()
@@ -18,8 +19,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         cardsArray = model.getCards()
+        
+        // Set the view controller as the datasource and delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
+    
+    // MARK: - Collection View Delegete Methods
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: configure it
+        
+        
+        return cell
+    }
 
 }
 
