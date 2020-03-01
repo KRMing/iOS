29c92d6d915baaa8cee8c1742eaba777c10dbf6c
KRMing <mkleeboy3@naver.com>
2020-03-01 20:12:40 +0900

    starting new practice Quiz App

---
+++ Implementation Practice/QuizApp(Practice)/QuizApp(Practice)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp(Practice)
+//
+//  Created by 이민건 on 3/1/20.
+//  Copyright © 2020 KRMing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
