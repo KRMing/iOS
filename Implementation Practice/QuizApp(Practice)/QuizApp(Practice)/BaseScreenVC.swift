3da7a1dfb2546d30ca248e2755ff299907e95cc1
KRMing <mkleeboy3@naver.com>
2020-03-01 21:09:58 +0900

    added QuizModelDelegate to BaseScreenVC

--- Implementation Practice/QuizApp(Practice)/QuizApp(Practice)/BaseScreenVC.swift
+++ Implementation Practice/QuizApp(Practice)/QuizApp(Practice)/BaseScreenVC.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
-class BaseScreenVC: UIViewController {
-
+class BaseScreenVC: UIViewController, QuizModelDelegate {
+    
+    var quizModel = QuizModel()
+    var quizData: [QuizFormat]!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
         
+        super.viewDidLoad()
+        
+        quizModel.quizModelDelegate = self
     }
+    
+    // MARK: - Quiz Model Delegate Protocol
 
-
+    func fetchQuizElements(elementArray: [QuizFormat]) {
+        
+        quizData = elementArray
+        
+        print(quizData)
+    }
 }
 
