14cbce444bafa33a090be2555e7a78d5570a4deb
KRMing <mkleeboy3@naver.com>
2020-03-13 21:12:46 +0900

    completed lesson 2 (setup)

---
+++ Database/Module 3 A Photo App/Mingstagram/Mingstagram/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  Mingstagram
+//
+//  Created by 이민건 on 3/13/20.
+//  Copyright © 2020 KRMing. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func signOutTapped(_ sender: UIButton) {
+        
+        
+    }
+}
